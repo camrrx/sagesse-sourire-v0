a7edec55c3041a003a39881d122be33bc8c28d16
Camille Roux <Camille@MacBook-Pro-de-MacBook.local>
2023-07-28 16:43:35 +0200

    test4

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import "./Home.scss"
 
 const Home = () => {
     return (  
-        <div className="home-container"><h2>home</h2></div>
+        <div className="home-container"><h2>home</h2><h2>home</h2><h2>home</h2></div>
     );
 }
  
